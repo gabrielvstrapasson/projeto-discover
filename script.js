d0724c3196dee8eed8489d893d1827c40a77ca94
AA-Gabriel <gabri.vstrapasson@gmail.com>
2025-02-19 09:33:02 -0300

    Add toggle img in dark mode and light mode

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
 function toggleMode() {
   const html = document.documentElement
-  /*
-  if (body.classList.contains("light")) {
-    HTMLAllCollection.classList.remove("light")
-  } else {
-    HTMLAllCollection.classList.add("light")
-  } */
-
   html.classList.toggle("light")
+
+  // pegar a tag img
+  const img = document.querySelector("#profile img")
+
+  // substituir a image
+  if (html.classList.contains("light")) {
+    // se tiver a light mode adicionar a imagem light
+    img.setAttribute("src", "./assets/avatar-light.png")
+    img.setAttribute("alt", "Gabriel in Light Mode")
+  } else {
+    // se tiver sem light mode, manter a imagem normal
+    img.setAttribute("src", "./assets/avatar.png")
+    img.setAttribute("alt", "Gabriel in Dark Mode")
+  }
 }
